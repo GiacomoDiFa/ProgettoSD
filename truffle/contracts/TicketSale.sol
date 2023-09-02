15e0a7997f531acf1f43fa66b9be1c29e75a2524
MatteoMarcoM <matteomm00@gmail.com>
2023-09-02 15:35:57 +0200

    cleaning contract code, added readme, final commit

--- truffle/contracts/TicketSale.sol
+++ truffle/contracts/TicketSale.sol
@@ -1,14 +1,9 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// prova
 contract TicketSale {
     address public owner;
-    //uint256 public totalTickets;
-    //uint256 public ticketsSold;
     uint256 public totalEvents;
-    //uint256 public newEventId;
-
+    
     struct Event {
         string name;
         string description;
@@ -31,18 +26,13 @@ contract TicketSale {
 
     constructor() {
         owner = msg.sender;
-        //totalTickets = _totalTickets;
-        //ticketsSold = 0;
         totalEvents = 0;
-        //newEventId = 0;
     }
 
     function createEvent(string memory name, string memory description, uint256 _totalTickets, uint256 ticketPrice) external onlyOwner {
         require(_totalTickets > 0, "Number of total tickets must be > 0");
         
-        // ID evento
-        //newEventId++;
-        //events[newEventId] = Event(name, description, _totalTickets, 0, ticketPrice);
+        // totalEvents e' l'ID evento
         events[totalEvents] = Event(name, description, _totalTickets, 0, ticketPrice);
         totalEvents++;
     }
@@ -51,25 +41,21 @@ contract TicketSale {
         Event storage currentEvent = events[eventId];
         require(currentEvent.totalTickets > 0, "Event does not exist");
         require(numTickets > 0, "You must buy at least one ticket");
-        //require(ticketsSold + numTickets <= totalTickets, "Not enough tickets available");
         require(currentEvent.ticketsSold + numTickets <= currentEvent.totalTickets, "Not enough tickets available for this event");
         require(msg.value >= currentEvent.ticketPrice * numTickets, "Insufficient payment");
 
         ticketsPurchased[msg.sender][eventId] += numTickets;
-        //ticketsSold += numTickets;
         currentEvent.ticketsSold += numTickets;
 
         uint amountToSend = currentEvent.ticketPrice * numTickets;
         uint change = msg.value - amountToSend;
-        // gli ether dentro msg.value sono tutti inviati a allo SC (default)
-        //payable(address(this)).transfer(amountToSend); 
-        // resto al mittente
+        // gli ether dentro msg.value sono tutti inviati allo SC (default)
+        // il resto al mittente
         payable(msg.sender).transfer(change);
 
         emit TicketPurchased(msg.sender, eventId, numTickets);
     }
 
-    // da rivedere
     event TicketReselled(address indexed seller, address indexed buyer, uint256 eventId, uint256 numTickets);
 
     // chi chiama la funzione paga ed e' il buyer
@@ -81,7 +67,7 @@ contract TicketSale {
         require(ticketsPurchased[seller][eventId] >= numTickets, "Not enough tickets available");
         require(msg.value >= currentEvent.ticketPrice * numTickets, "Insufficient payment");
 
-        // NB funzioni successive
+        // vedi funzioni successive
         require(ticketsToResell[seller][eventId] >= numTickets, "Not enough tickets to resell");
 
         ticketsToResell[seller][eventId] -= numTickets;
@@ -89,12 +75,12 @@ contract TicketSale {
         ticketsPurchased[seller][eventId] -= numTickets;
         ticketsPurchased[msg.sender][eventId] += numTickets;
         
-        //NB msg.value va tutto allo SC che poi inoltra i soldi
+        // msg.value va tutto allo SC che poi inoltra i soldi
         uint amountToSend = currentEvent.ticketPrice * numTickets;
         uint change = msg.value - amountToSend;
         // Transfer the amount to the seller
         payable(seller).transfer(amountToSend); 
-        // resto al mittente
+        // il resto al mittente
         payable(msg.sender).transfer(change);
 
         emit TicketReselled(seller, msg.sender, eventId, numTickets);
@@ -112,13 +98,12 @@ contract TicketSale {
         emit WantToResell(msg.sender, eventId, _numTickets);
     }
 
-    // getter per il msg.sender (facoltativo)
+    // getter per il msg.sender
     function getMyTicketsToResell(uint256 eventId) external view returns (uint256) {
         return ticketsToResell[msg.sender][eventId];
     }
 
-    // da rivedere se funziona
-    // rimborso ticket (only owner?)
+    // rimborso ticket
     function refundTickets(uint256 eventId, uint256 numTickets) external {
         Event storage currentEvent = events[eventId];
         require(currentEvent.totalTickets > 0, "Event does not exist");
@@ -130,11 +115,9 @@ contract TicketSale {
         require(balance >= refundAmount, "Smart Contract balance is not sufficient");
 
         ticketsPurchased[msg.sender][eventId] -= numTickets;
-        //ticketsSold -= numTickets;
         currentEvent.ticketsSold -= numTickets;
 
-        // Transfer the refund amount to the buyer           
-        // Funziona (paga lo SC)
+        // Transfer the refund amount to the buyer (paga lo SC)
         payable(msg.sender).transfer(refundAmount);
     }
 
@@ -148,22 +131,6 @@ contract TicketSale {
         require(balance > 0, "Contract has no balance");
         payable(msg.sender).transfer(balance);
     }
-
-    // MI SA CHE NON HA MOLTO SENSO, LO TOGLIAMO?
-    /*
-    // single event
-    function withdrawFunds(uint256 eventId) external onlyOwner {
-        Event storage currentEvent = events[eventId];
-        require(currentEvent.totalTickets > 0, "Event does not exist");
-
-        // i soldi vengono dal balance dello SC
-        uint256 totalRefund = currentEvent.ticketPrice * currentEvent.ticketsSold;
-        uint256 balance = address(this).balance;
-        require(balance >= totalRefund, "Contract has not enough balance");
-    
-        payable(msg.sender).transfer(totalRefund);
-    }
-    */
 
     function getMyTicketCount(uint256 eventId) external view returns (uint256) {
         return ticketsPurchased[msg.sender][eventId];
@@ -198,10 +165,6 @@ contract TicketSale {
         return this.totalTickets() - this.totalTicketsSold();
     }
     
-    // funzioni in piu' (opzionali) CHAT GPT
-    // sono troppe?
-
-    // modifica evento
     function editEvent(uint256 eventId, string memory name, string memory description, uint256 _totalTickets, uint256 _ticketPrice) external onlyOwner {
         Event storage currentEvent = events[eventId];
         require(currentEvent.totalTickets > 0, "Event does not exist");
@@ -212,20 +175,7 @@ contract TicketSale {
         currentEvent.totalTickets = _totalTickets;
         currentEvent.ticketPrice = _ticketPrice;
     }
-
-    /*
-    // lista eventi
-    // e' corretta? serve?
-    function listEvents() external view returns (Event[] memory) {
-        Event[] memory eventList = new Event[](totalEvents);
-        for (uint256 i = 0; i < totalEvents; i++) {
-            eventList[i] = events[i];
-        }
-        return eventList;
-    }
-    */
-
-    // sold out event
+    
     function isEventSoldOut(uint256 eventId) external view returns (bool) {
         Event storage currentEvent = events[eventId];
         return currentEvent.ticketsSold >= currentEvent.totalTickets;
@@ -234,9 +184,5 @@ contract TicketSale {
     function getTotalEvents() external view returns (uint256){
         return totalEvents;
     }
-
-    function getTicketsToResellForAddressAndEvent(address _address, uint256 eventId) external view returns (uint256) {
-    return ticketsToResell[_address][eventId];
-}
 
 }
